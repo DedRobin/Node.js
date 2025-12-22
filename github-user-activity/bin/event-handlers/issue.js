e15248c151bbacdeb837fd3667815790b8a6e8b4
Pavel Rabekin <pavelrabekin@gmail.com>
2025-12-22 15:46:40 +0300

    feat: add issueComment handler

--- github-user-activity/bin/event-handlers/issue.js
+++ github-user-activity/bin/event-handlers/issue.js
@@ -1,7 +1,29 @@
 const { print } = require('./services');
 
+const ISSUE_ACTION_TYPE = {
+  opened: 'Opened',
+  closed: 'Closed',
+  reopened: 'Reopened',
+};
+
 const handleIssue = event => {
-  const message = 'Opened, closed, or managed an issue';
+  const actionType = event?.payload?.action;
+  if (!actionType) throw new Error('No issue action type');
+  const action = ISSUE_ACTION_TYPE[actionType];
+
+  const title = event?.payload?.issue?.title;
+  if (!title) throw new Error('No issue title');
+
+  const description = event?.payload?.issue?.body;
+  if (!description) throw new Error('No issue body');
+
+  const url = event?.payload?.issue?.['html_url'];
+  if (!url) throw new Error('No issue url');
+
+  const message = `${action} the issue
+Title: ${title}
+Description: ${description}
+URL: ${url}`;
 
   print(message, event);
 };
--- github-user-activity/bin/event-handlers/issueComment.js
+++ github-user-activity/bin/event-handlers/issueComment.js
@@ -1,7 +1,22 @@
 const { print } = require('./services');
 
 const handleIssueComment = event => {
-  const message = 'Posted a comment on an issue';
+  const whoseComment = event?.payload?.issue?.user?.login;
+  if (!whoseComment) throw new Error('No user who leaves the comment');
+
+  const title = event?.payload?.issue?.title;
+  if (!title) throw new Error('No issue title');
+
+  const url = event?.payload?.issue?.['html_url'];
+  if (!url) throw new Error('No issue url');
+
+  const comment = event?.payload?.issue?.body;
+  if (!comment) throw new Error('No issue body');
+
+  const message = `Posted a comment on an issue by ${whoseComment}
+Title: ${title}
+Comment: ${comment}
+URL: ${url}`;
   print(message, event);
 };
 
