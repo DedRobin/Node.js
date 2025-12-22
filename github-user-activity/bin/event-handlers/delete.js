e0fc2c42b9511961f4f7b6cd5428d66faacafb7c
Pavel Rabekin <pavelrabekin@gmail.com>
2025-12-22 11:51:40 +0300

    feat: add delete hanlder

--- github-user-activity/bin/event-handlers/delete.js
+++ github-user-activity/bin/event-handlers/delete.js
@@ -1,7 +1,14 @@
 const { print } = require('./services');
 
 const handleDelete = event => {
-  const message = 'Deleted a branch or tag';
+  const refType = event?.payload?.['ref_type'];
+  if (!refType) throw new Error('No ref type');
+
+  const deletedBranch = event?.payload?.ref;
+  if (!deletedBranch) throw new Error('No deleted branch');
+
+  const message = `Deleted the ${refType}
+Removed branch: ${deletedBranch}`;
 
   print(message, event);
 };
