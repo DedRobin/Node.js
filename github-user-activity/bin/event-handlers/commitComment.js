5a6ef2f013f30471c7e733ff721f91cdaf18eafb
Pavel Rabekin <pavelrabekin@gmail.com>
2025-12-22 11:44:13 +0300

    feat: add commitComment handler

--- github-user-activity/bin/event-handlers/commitComment.js
+++ github-user-activity/bin/event-handlers/commitComment.js
@@ -1,7 +1,19 @@
 const { print } = require('./services');
 
 const handleCommitComment = event => {
-  const message = 'Commented on a commit';
+  const comment = event?.payload?.comment?.['body'];
+  if (!comment) throw new Error('No comment');
+
+  const url = event?.payload?.comment?.['html_url'];
+  if (!url) throw new Error('No comment URL');
+
+  const action = event?.payload?.['action'];
+  if (!url) throw new Error('No comment action');
+
+  const message = `Commented on a commit
+Action=${action}
+Comment: ${comment}
+URL: ${url}`;
 
   print(message, event);
 };
