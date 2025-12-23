b182667e9f28638511fb0619e718797a874a77fd
Pavel Rabekin <pavelrabekin@gmail.com>
2025-12-23 14:23:29 +0300

    feat: add fork handler

--- github-user-activity/bin/event-handlers/fork.js
+++ github-user-activity/bin/event-handlers/fork.js
@@ -1,7 +1,13 @@
 const { print } = require('./services');
 
 const handleFork = event => {
-  const message = 'Forked a repository';
+  const forkedRepoUrl = event?.payload?.forkee?.['clone_url'];
+  if (!forkedRepoUrl) throw new Error('No forked repository');
+  const forkedRepoName = event?.payload?.forkee?.['full_name'];
+  if (!forkedRepoName) throw new Error('No forked repository');
+
+  const message = `Forked a repository '${forkedRepoName}'
+URL: ${forkedRepoUrl}`;
   print(message, event);
 };
 
