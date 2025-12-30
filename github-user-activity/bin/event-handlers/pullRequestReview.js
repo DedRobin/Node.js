0d53e026dde12e8b0139faa5136aa8a5eb3eb33c
Pavel Rabekin <pavelrabekin@gmail.com>
2025-12-30 15:55:53 +0300

    fix: fix PR review handler

--- github-user-activity/bin/event-handlers/pullRequestReview.js
+++ github-user-activity/bin/event-handlers/pullRequestReview.js
@@ -1,10 +1,10 @@
 const { print } = require('./services');
 
 const handlePullRequestReview = event => {
-  const reviewer = event?.payload?.user?.login;
+  const reviewer = event?.payload?.review?.user?.login;
   if (!reviewer) throw new Error('No reviewer');
 
-  const url = event?.payload?.['html_url'];
+  const url = event?.payload?.review?.['html_url'];
   if (!url) throw new Error('No review URL');
 
   const message = `Submitted a review for a pull request by ${reviewer}
